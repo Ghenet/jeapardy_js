7b45510ba5dc11adab51fb30ef7a19ae102934b3
Ghenet Andemichael <38365838+Ghenet@users.noreply.github.com>
2021-10-21 14:46:32 -0700

    Update script.js
    make api call for each level to fetch the json data

--- script.js
+++ script.js
@@ -8,4 +8,15 @@ const levels = ['easy', 'medium', 'hard']
 function addGenre() {
     const column = document.createElement('div');
     column.classList.add('genre-column');
+    column.innerHTML = "this is genre";
+    game.append(column); //add the column to the game div
+
+    //lets make a call to the api for each level
+    levels.forEach((level) => {
+        fetch(`https://opentdb.com/api.php?amount=10&category=11&difficulty=${level}&type=boolean`)
+            .then(res => responce.json())
+            .then(data => console.log(data));
+    })
 }
+
+addGenre();
